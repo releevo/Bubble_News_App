47da3ed5956e214c4a5e611f75bc93ed1f73e290
Alan <allen0lee@gmail.com>
2019-05-21 11:07:04 +1000

    setup modles and config files

--- app_console.rb
+++ app_console.rb
@@ -1,4 +1,11 @@
 require 'pry'
-require 'db_config'
+require_relative 'db_config'
+
+require_relative 'models/vote.rb'
+require_relative 'models/discussion.rb'
+require_relative 'models/article.rb'
+require_relative 'models/user.rb'
+require_relative 'models/topic.rb'
+require_relative 'models/story.rb'
 
 binding.pry
--- db_config.rb
+++ db_config.rb
@@ -3,7 +3,7 @@ require 'active_record'
 
 options = {
     adapter: 'postgresql',
-    # database:
+    database: 'bubble_news_app'
 }
 
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || options)
