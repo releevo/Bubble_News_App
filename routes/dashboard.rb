6e7ee1083523a1535d40bb2e7e4a88e9c65c57c0
Roisin <roisin.briscoe@gmail.com>
2019-05-22 16:35:58 +1000

    dashboard section set up and functional waiting on login and edit route

--- routes/dashboard.rb
+++ routes/dashboard.rb
@@ -1,7 +1,18 @@
 # dashboard
 
 get '/users/:id' do
-  redirect '/login' unless logged_in?
-  @notifications = Notification.find_by(recevier_id: session[:user_id])
+  # redirect '/login' unless logged_in?
+  @notifications = Notification.where(receiver_id: 1)
   erb :users
 end
+
+delete '/notifications/:id' do
+  notification = Notification.find(params[:id])
+  notification.delete
+  redirect '/users'
+end
+
+#Need to add current_user.id
+
+# session[:user_id]
+# find_by - only finds the first one like querySelector
