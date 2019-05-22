5ab23f035356969d44dd194445628e78d6df39a5
Roisin <roisin.briscoe@gmail.com>
2019-05-22 12:17:14 +1000

    notification, story-topic, story-article models added and added to app console and main

--- app_console.rb
+++ app_console.rb
@@ -7,5 +7,9 @@ require_relative 'models/article.rb'
 require_relative 'models/user.rb'
 require_relative 'models/topic.rb'
 require_relative 'models/story.rb'
+require_relative 'models/story_topic.rb'
+require_relative 'models/story_article.rb'
+require_relative 'models/notification.rb'
+
 
 binding.pry
--- main.rb
+++ main.rb
@@ -11,6 +11,16 @@ require_relative 'routes/login.rb'
 require_relative 'routes/signup.rb'
 require_relative 'routes/view_story.rb'
 
+require_relative 'models/vote.rb'
+require_relative 'models/discussion.rb'
+require_relative 'models/article.rb'
+require_relative 'models/user.rb'
+require_relative 'models/topic.rb'
+require_relative 'models/story.rb'
+require_relative 'models/story_topic.rb'
+require_relative 'models/story_article.rb'
+require_relative 'models/notification.rb'
+
 enable :sessions
 
 helpers do
