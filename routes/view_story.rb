27443af1c7a95c33acdae9f9e9648f4a0b714a92
Milo Zheng <milozhengz@gmail.com>
2019-05-22 14:18:26 +1000

    dt said so

--- routes/view_story.rb
+++ routes/view_story.rb
@@ -4,6 +4,6 @@ get '/stories/:id' do
   @story = Story.find(params[:id])
   story_articles = @story.stories_articles
   @article_original = story_articles.first.article
-  @article_perspective = story_article
+  @article_perspective = story_article.last.article
   erb :show
 end
