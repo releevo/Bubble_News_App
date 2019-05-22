243da8a1a76037f4ac5a4d573e3e561d2bb4e026
Milo Zheng <milozhengz@gmail.com>
2019-05-22 16:38:28 +1000

    view story - add discussion feature complete.

--- routes/view_story.rb
+++ routes/view_story.rb
@@ -2,8 +2,20 @@
 
 get '/stories/:id' do
   @story = Story.find(params[:id])
+  @story_creator = User.find(@story.creator_id)
   story_articles = @story.stories_articles
   @article_original = story_articles.first.article
   @article_perspective = story_articles.last.article
+  @perspective_user = User.find(@article_perspective.user_id)
+  @discussions = Discussion.where(story_id: @story.id)
   erb :show
+end
+
+post '/discussions' do
+  @discussion = Discussion.new
+  @discussion.user_id = 3
+  @discussion.content = params[:content]
+  @discussion.story_id = params[:story_id]
+  @discussion.save
+  redirect "/stories/#{@discussion.story_id}"
 end
