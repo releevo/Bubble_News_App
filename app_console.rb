490be9fac582b4f8b46b54196d352c888e52ab03
Milo <milozhengz@gmail.com>
2019-06-19 14:47:39 +1000

    remove discussion from require

--- app_console.rb
+++ app_console.rb
@@ -2,7 +2,7 @@ require 'pry'
 require_relative 'db_config'
 
 require_relative 'models/vote.rb'
-require_relative 'models/discussion.rb'
+require_relative 'models/comment.rb'
 require_relative 'models/article.rb'
 require_relative 'models/user.rb'
 require_relative 'models/topic.rb'
