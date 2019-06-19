f673b9c7058b9b8dbf74387e2eba8775cc49034d
Alan <allen0lee@gmail.com>
2019-06-19 13:46:04 +1000

    add UsersTopic model and update add_story route

--- routes/add_story.rb
+++ routes/add_story.rb
@@ -10,12 +10,12 @@ post '/new' do
   story.creator_id = current_user.id
   story.time_created = Time.now.strftime("%d/%m/%Y %H:%M")
   story.save
-
-  topic = Topic.new
-  topic.name = params[:chosen_topics]
-  topic.user_id = current_user.id
-  topic.story_id = story.id
-  topic.save
+  
+  params[:chosen_topics].each do |chosen_topic_id|
+    stories_topic = StoriesTopic.new
+    stories_topic.topic_id = chosen_topic_id
+    stories_topic.story_id = story.id
+  end
 
   #these params need to be updated as per the URL search Alan
   article = Article.new
@@ -24,38 +24,32 @@ post '/new' do
   article.article_description = params[:article_description]
   article.image_url = params[:article_image_url]
   article.user_id = current_user.id
+  article.original_side = true
   article.save
 
   stories_article = StoriesArticle.new
   stories_article.article_id = article.id
   stories_article.story_id = story.id
-  stories_article.original_side = true
-  stories_article.contributor_id = current_user.id
+  # stories_article.contributor_id = current_user.id
   stories_article.save
-
-  stories_topic = StoriesTopic.new
-  stories_topic.topic_id = topic.id
-  stories_topic.story_id = story.id
-  stories_topic.save
-
 
   # get all user ids with a connection
   # limit 10
   # pluck user_id
   # each
 
-  topic_selection = Topic.where.not(user_id: current_user.id).where(name: topic.name).limit(10).pluck(:user_id)
-  topic_selection.each do |user_id|
-    notification = Notification.new
-    notification.sender_id = current_user.id
-    notification.receiver_id = user_id
-    notification.story_id = story.id
-    notification.article_id = article.id
-    notification.notification_type = "invite"
-    notification.seen = false
-    notification.time_created = Time.now
-    notification.save
-  end
+  # topic_selection = UsersTopic.where.not(user_id: current_user.id).where(name: topic.name).limit(10).pluck(:user_id)
+  # topic_selection.each do |user_id|
+  #   notification = Notification.new
+  #   notification.sender_id = current_user.id
+  #   notification.receiver_id = user_id
+  #   notification.story_id = story.id
+  #   notification.article_id = article.id
+  #   notification.notification_type = "invite"
+  #   notification.seen = false
+  #   notification.time_created = Time.now
+  #   notification.save
+  # end
 
   # erb :index
   # redirect "/stories/#{story.id}"
