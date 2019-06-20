6ecaa9f7808aaa8d945fc72ab98881cbd53f1384
Roisin <roisin.briscoe@gmail.com>
2019-06-20 10:26:32 +1000

    updating user_topic model to relate to users and topics

--- models/topic.rb
+++ models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ActiveRecord::Base
     belongs_to :user, class_name: 'User', foreign_key: 'user_id'
     belongs_to :story, class_name: 'Story', foreign_key: 'story_id'
+<<<<<<< a395efdeebd0d00fe1a86f75c9228ceaf3d5a9ef
     # belongs_to :story_title, class_name: 'StoriesArticle', foreign_key: 'story_id'  
     # belongs_to :story_vote, class_name: 'Vote', foreign_key: 'story_id'
 
@@ -23,3 +24,6 @@ end
 
 # false
 # {title: "no story"}
+=======
+end
+>>>>>>> updated users_topic model with relationships
