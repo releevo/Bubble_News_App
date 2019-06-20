98269ac6bdfe5773d47b196b20f88fc6814d7756
Roisin <roisin.briscoe@gmail.com>
2019-06-20 11:51:47 +1000

    create new notifications has been fixed whithin add story and schema updated for correct foreign key

--- models/topic.rb
+++ models/topic.rb
@@ -1,7 +1,6 @@
 class Topic < ActiveRecord::Base
     belongs_to :user, class_name: 'User', foreign_key: 'user_id'
     belongs_to :story, class_name: 'Story', foreign_key: 'story_id'
-<<<<<<< a395efdeebd0d00fe1a86f75c9228ceaf3d5a9ef
     # belongs_to :story_title, class_name: 'StoriesArticle', foreign_key: 'story_id'  
     # belongs_to :story_vote, class_name: 'Vote', foreign_key: 'story_id'
 
@@ -24,6 +23,3 @@ end
 
 # false
 # {title: "no story"}
-=======
-end
->>>>>>> updated users_topic model with relationships
