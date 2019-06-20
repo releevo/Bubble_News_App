604f4e60975479b294a8b8a6dfcded76b9ef246f
Roisin <roisin.briscoe@gmail.com>
2019-06-20 15:16:28 +1000

    notifications page showing article title and able to accept and ingore

--- models/notification.rb
+++ models/notification.rb
@@ -12,7 +12,3 @@ class Notification < ActiveRecord::Base
     end
 
 end
-
-# Vote.where(story_id: self.id).count
-
-# doesnt equal the the same column name it equals the foreign key (id in the first column) therefore the above doesnt work.  Need to get the uniq id use where instead of traversing the tables. I have the user id and the story id to get to the article title.  If you do it on user it could be a different article that is linked to them. Do a where to get the info from that table.
