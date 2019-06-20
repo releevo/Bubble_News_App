1f11d1dbe587d4aac70312523f257f453bf45b0c
Milo <milozhengz@gmail.com>
2019-06-20 14:26:54 +1000

    clean up code for homepage

--- routes/homepage.rb
+++ routes/homepage.rb
@@ -2,37 +2,7 @@
 
 get '/' do
   @topics = Topic.all
-
-  # @topics.each do |topic|
-  #   @topic_story = topic.story_with_most_votes
-  #   topic_story_articles = topic_story.stories_articles
-  #   @topic_article_original = topic_story_articles.first.article
-  # end
-
-  # @politics = Topic.find(1)
-  # @politics_story = @politics.story_with_most_votes
-  # @politics_story_articles = @politics_story.stories_articles
-  # @politics_article_original = @politics_story_articles.first.article
   erb :index
 end
-
-#Need to find the highest ranked article and display on home page. This is the sum of two votes. Use relationship.
-
-#Need to get each topic inyp erb
-
-#@story = story where story_id = stories article story_id
-
-#entertainment.story.title
-
-#loop through the topics, retrieve the stories for each topic, retrieve the count of votes for each story
-
-# @politics = Topic.where(name: "Politics")
-  # @economy = Topic.where(name: "Economy")
-  # @environment = Topic.where(name: "Environment")
-  # @science = Topic.where(name: "Sceince")
-  # @culture = Topic.where(name: "Culture")
-  # @sports = Topic.where(name: "Sport")
-  # @society = Topic.where(name: "Society")
-  # @entertainment = Topic.where(name: "Entertainment")
 
 
