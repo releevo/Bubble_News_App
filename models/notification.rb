3d045211764a4dd618da8442863bc59f99398986
Roisin <roisin.briscoe@gmail.com>
2019-06-21 12:03:56 +1000

    notifications style complete and notification errors resolved

--- models/notification.rb
+++ models/notification.rb
@@ -1,8 +1,6 @@
 class Notification < ActiveRecord::Base
     belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
     belongs_to :sender_story, class_name: 'Story', foreign_key: 'story_id'
-    # belongs_to :sender_article, class_name: 'Article', foreign_key: 'article_id'
-    # belongs_to :sender_article_one, class_name: 'StoriesArticle', foreign_key: 'story_id'
 
     def article_title
       story_article_record = StoriesArticle.where(story_id: self.story_id)
