925370ba8f9e9edffbf1065bbe77cba648b42910
Milo <milozhengz@gmail.com>
2019-06-21 15:23:10 +1000

    homapage topic nav working - need to resolve haandlebar

--- routes/homepage.rb
+++ routes/homepage.rb
@@ -5,4 +5,25 @@ get '/' do
   erb :index
 end
 
+get '/api' do
+  topic = Topic.find(params[:topic_id])
+  topic_stories = []
+  topic_stories_ids = topic.stories_topics
+  topic_stories_ids.each do |topic| 
+    topic_stories << Story.find(topic.story_id)
+  end
+  topic_stories_articles = []
+  topic_stories.each do |story|
+    topic_stories_articles << story.stories_articles.first.article
+  end
+  content_type :json
+  {
+    topic: topic,
+    topic_stories: topic_stories,
+    topic_stories_articles: topic_stories_articles
+  }.to_json
+end
 
+
+# article description
+# article
